a8fddf450cc3f23308aa15834303efdd3c5fb330
Alain <alain18lozano@gmail.com>
2025-06-30 01:54:46 -0600

    ArregloPC-ANDROID2

--- src/App.jsx
+++ src/App.jsx
@@ -16,13 +16,14 @@ function App() {
 
       <div className="video-container">
         <iframe
-          src="https://www.youtube.com/embed/Bv-1BnoB75k?autoplay=1&mute=0"
+          src="https://www.youtube.com/embed/Bv-1BnoB75k?autoplay=1&mute=1"
           title="YouTube video player"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           referrerPolicy="strict-origin-when-cross-origin"
           allowFullScreen
         ></iframe>
       </div>
+
 
 
       <div className="card">
