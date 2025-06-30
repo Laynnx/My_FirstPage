82490468a4013e6d1b8fe5c68700202e80e43db7
Alain <alain18lozano@gmail.com>
2025-06-30 01:26:29 -0600

    Iframe

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
 import { useState } from 'react'
 import './App.css'
+import jojiimg from './assets/logo11.png'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <div>
+        <a href="https://www.youtube.com/@joji" target="_blank">
+          <img  src={jojiimg} className="logo" alt="Vite logo" />
+        </a>
+      </div>
       <h1>Alain Pag</h1>
+
+      <div className="video-container">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/Bv-1BnoB75k?autoplay=1&mute=0"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+      </div>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
