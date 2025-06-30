34692350a7d47a076dc68e1a2bfcbc76f20a6c8b
Alain <alain18lozano@gmail.com>
2025-06-30 01:45:56 -0600

    ArregloPC-ANDROID

--- src/App.jsx
+++ src/App.jsx
@@ -16,16 +16,14 @@ function App() {
 
       <div className="video-container">
         <iframe
-          width="560"
-          height="315"
           src="https://www.youtube.com/embed/Bv-1BnoB75k?autoplay=1&mute=0"
           title="YouTube video player"
-          frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           referrerPolicy="strict-origin-when-cross-origin"
           allowFullScreen
         ></iframe>
       </div>
+
 
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
